e8cd8f812116b9f2f963d7d5a0c36b95d6cf7d73
atilayeseN <38512694+atilayeseN@users.noreply.github.com>
2021-02-02 23:59:05 +0300

    Create source.java

---
+++ source.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+public static void main(String[] args) {
+	int[] arr= {1,2,3,4,5};
+	int[] sol=new int[2]; 
+	int target;
+	Scanner scn=new Scanner(System.in);
+	
+	target=scn.nextInt();
+	sol=solution(arr,target);	
+	for(int i=0;i<2;i++) {
+		System.out.println(sol[i]);
+	}
+	
+}
+public static int[] solution(int[] arr,int target) {
+	int[] sol=new int[2]; 
+	for(int i=0;i<5;i++) {
+		for(int j=0;j<5;j++) {
+			if(arr[i] + arr[j]==target) {
+				sol[0]=arr[j];
+				sol[1]=arr[i];
+			}
+		    
+		}
+	}
+	return sol;
+}
+
+}
