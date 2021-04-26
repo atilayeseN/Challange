5cdfbe640e9122c454ce7dda9cbdde0a7aacb9b2
atilayeseN <38512694+atilayeseN@users.noreply.github.com>
2021-04-26 15:15:24 +0300

    Varible name updates and prepare and convention

--- Main.java
+++ Main.java
@@ -3,34 +3,34 @@ import java.util.Scanner;
 public class Main {
 public static void main(String[] args) {
 	int[] arr= {1,2,3,4,5};
-	int[] sol=new int[2]; 
+	int[] solutionArray=new int[2]; 
 	int target;
 	System.out.println("Lütfen bir sayı girin");
 	Scanner scn=new Scanner(System.in);
 	
 	target=scn.nextInt();
-	sol=solution(arr,target);	
-	if(target>10 || target<2) {
+	solutionArray=solution(arr,target);	
+	if((target>10) || (target<2)) {
 		throw new IllegalArgumentException("Lütfen geçerli sayılar girin sayılar girin");
 	}
 	System.out.println("İndexler");
 	for(int i=0;i<2;i++) {
-		System.out.println(sol[i]);
+		System.out.println(solutionArray[i]);
 	}
 	
 }
 public static int[] solution(int[] arr,int target) {
-	int[] sol=new int[2]; 
+	int[] resultArray=new int[2]; 
 	for(int i=0;i<5;i++) {
 		for(int j=0;j<5;j++) {
 			if(arr[i] + arr[j]==target) {
-				sol[0]=j;
-				sol[1]=i;
+				resultArray[0]=j;
+				resultArray[1]=i;
 			}
 		    
 		}
 	}
-	return sol;
+	return resultArray;
 }
 
 }
